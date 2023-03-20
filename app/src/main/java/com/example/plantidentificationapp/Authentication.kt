e1588f30d7348d9ddb37f436f192bcfb3ceb4d09
KacperSynator <kacper.synator@gmail.com>
2023-03-20 21:27:49 +0100

    Firebase: replace depracated functions and add sign out functionality

--- app/src/main/java/com/example/plantidentificationapp/Authentication.kt
+++ app/src/main/java/com/example/plantidentificationapp/Authentication.kt
@@ -1,5 +1,82 @@
 package com.example.plantidentificationapp
 
-class Authentication {
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.login_screen.*
 
+class Authentication : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        // Configure Google Sign-In Options
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        // Create Google Sign-In Client
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Set onClickListener for Google Sign-In Button
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            signInLauncher.launch(signInIntent)
+        }
+
+        // Set onClickListener for Sign-Out Button
+        signOutButton.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                // Google Sign-In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Google Sign-In failed
+                Toast.makeText(this, "Google Sign-In failed.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    val user = mAuth.currentUser
+                    Toast.makeText(this, "Welcome, ${user?.displayName}", Toast.LENGTH_SHORT).show()
+                } else {
+                    // Sign in fails
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun signOut() {
+        mAuth.signOut()
+        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+            // Sign out success
+            Toast.makeText(this, "Signed out successfully.", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
