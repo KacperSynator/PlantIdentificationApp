2b9c4b2ae203ca957dc7b4f91348990b6ad6c8c1
KacperSynator <kacper.synator@gmail.com>
2023-04-01 23:13:52 +0200

    Firebase: move Authentication logic to primitive class AuthenticationManager

---
+++ app/src/main/java/com/example/plantidentificationapp/AuthenticationManager.kt
@@ -0,0 +1,98 @@
+package com.example.plantidentificationapp
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+class AuthenticationManager(private val context: Context) {
+    val logTag = "AuthenticationManager"
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    private val googleSignInClient by lazy {
+        GoogleSignIn.getClient(
+            context, GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(context.getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        )
+    }
+
+    fun createSignInIntent(): Intent {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(context, gso)
+
+        return googleSignInClient.signInIntent
+    }
+
+    fun currentUser(): FirebaseUser? {
+        return auth.currentUser
+    }
+
+    fun createAccountWithEmailAndPassword(
+        email: String,
+        password: String,
+        callback: (success: Boolean) -> Unit
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.e(logTag, "Email and password user creation failed: ${task.exception}")
+                }
+
+                callback(task.isSuccessful)
+            }
+    }
+
+    fun logInWithEmailAndPassword(
+        email: String,
+        password: String,
+        callback: (success: Boolean) -> Unit
+    ) {
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e(logTag, "Email and password login failed: ${task.exception}")
+            }
+
+            callback(task.isSuccessful)
+        }
+    }
+
+    fun logInWithGoogle(
+        acct: GoogleSignInAccount,
+        callback: (success: Boolean) -> Unit
+    ) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+
+        auth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e(logTag, "Google authentication failed: ${task.exception}")
+            }
+
+            callback(task.isSuccessful)
+        }
+    }
+
+    fun signOut(callback: (success: Boolean) -> Unit) {
+        auth.signOut()
+
+        googleSignInClient.signOut().addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e(logTag, "Google sign out: ${task.exception}")
+            }
+
+            callback(task.isSuccessful)
+        }
+    }
+}
--- app/src/main/java/com/example/plantidentificationapp/MainActivity.kt
+++ app/src/main/java/com/example/plantidentificationapp/MainActivity.kt
@@ -1,12 +1,121 @@
 package com.example.plantidentificationapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import kotlinx.android.synthetic.main.login_screen.*
 
 class MainActivity : AppCompatActivity() {
+    private val logTag = "MainActivity"
+
+    private lateinit var authManager: AuthenticationManager
+
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+
+                val responseCallback: (Boolean) -> Unit = { success ->
+                    toastResult(
+                        success,
+                        "Welcome, ${authManager.currentUser()?.displayName}",
+                        "Authentication failed."
+                    )
+                }
+
+                authManager.logInWithGoogle(account, responseCallback)
+
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google Sign-In failed.", Toast.LENGTH_SHORT).show()
+                Log.e(logTag, "Google Sign-In failed.")
+            }
+        }
+
+    data class GetEmailAndPasswordResult(
+        val success: Boolean, val email: String, val password: String
+    )
+
+    private fun getEmailAndPassword(): GetEmailAndPasswordResult {
+        val email = login_edit_email.text.toString()
+        val password = login_edit_haslo.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            return GetEmailAndPasswordResult(false, email, password)
+        }
+
+        return GetEmailAndPasswordResult(true, email, password)
+    }
+
+    private fun toastResult(success: Boolean, onSuccess: String, onFailure: String) {
+        if (success) {
+            Toast.makeText(this, onSuccess, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, onFailure, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
+
+        authManager = AuthenticationManager(this)
+
+        logging_button.setOnClickListener {
+            val result = getEmailAndPassword()
+
+            if (!result.success) {
+                return@setOnClickListener
+            }
+
+            val responseCallback: (Boolean) -> Unit = { success ->
+                toastResult(
+                    success,
+                    "Welcome, ${authManager.currentUser()?.displayName}",
+                    "Authentication failed."
+                )
+            }
+
+            authManager.logInWithEmailAndPassword(result.email, result.password, responseCallback)
+        }
+
+        sign_up_button.setOnClickListener {
+            val result = getEmailAndPassword()
+
+            if (!result.success) {
+                return@setOnClickListener
+            }
+
+            val responseCallback: (Boolean) -> Unit = { success ->
+                toastResult(
+                    success, "Account created successfully", "Failed to create account."
+                )
+            }
+
+            authManager.createAccountWithEmailAndPassword(
+                result.email, result.password, responseCallback
+            )
+        }
+
+        google_sign_in_button.setOnClickListener {
+            val signInIntent = authManager.createSignInIntent()
+            signInLauncher.launch(signInIntent)
+        }
+
+        sign_out_button.setOnClickListener {
+            val responseCallback: (Boolean) -> Unit = { success ->
+                toastResult(
+                    success, "Signed out successfully.", "Signed out failed."
+                )
+            }
+
+            authManager.signOut(responseCallback)
+        }
     }
 }
-
